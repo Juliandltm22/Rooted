d0d93465c7f73e830adf6eddefe2c697690a2e5c
Julian De La Torre <112035671+Juliandltm22@users.noreply.github.com>
2026-08-10 21:58:45 -0700

    FIX: fixed input viewing issue

--- project/app/(auth)/login.tsx
+++ project/app/(auth)/login.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 import { Alert, Text, TextInput, TouchableOpacity, View, Image, KeyboardAvoidingView, Platform, ScrollView} from 'react-native'
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { router } from 'expo-router'
 import { supabase } from '../lib/supabase'
 import { appStyles } from '../../styles/styles'
@@ -33,92 +33,122 @@ export default function Login() {
     }
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: '#FCF9ED' }}>
-            <KeyboardAvoidingView
-                style={{ flex: 1, backgroundColor: '#FCF9ED' }}
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                >
-                    <ScrollView
-                        contentContainerStyle={{ flexGrow: 1, backgroundColor: '#FCF9ED' }}
-                        keyboardShouldPersistTaps="handled"
+        <View style={{ flex: 1, backgroundColor: '#FCF9ED' }}>
+            <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
+                <KeyboardAvoidingView
+                    style={{ flex: 1 }}
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
                     >
-                    <View style={{ position: 'relative' }}>
-                        <View style={{ paddingTop: 40, paddingLeft: 24 }}>
-                            <Text style={[appStyles.subtitleHeadline2, { lineHeight: 40 }]}>
-                                Continue Growing
-                            </Text>
-                            <Text style={[appStyles.subtitleHeadline2, { lineHeight: 40 }]}>
-                                Your Healthy 
-                            </Text>
-                            <Text style={[appStyles.subtitleHeadline2, { lineHeight: 40 }]}>
-                                Habits
-                            </Text>
+                        <ScrollView
+                            contentContainerStyle={{ flexGrow: 1, backgroundColor: '#FCF9ED' }}
+                            keyboardShouldPersistTaps="handled"
+                        >
+                        <View style={{ position: 'relative' }}>
+                            <View style={{ paddingTop: 40, paddingLeft: 24 }}>
+                                <Text style={[appStyles.subtitleHeadline2, { lineHeight: 40 }]}>
+                                    Continue Growing
+                                </Text>
+                                <Text style={[appStyles.subtitleHeadline2, { lineHeight: 40 }]}>
+                                    Your Healthy 
+                                </Text>
+                                <Text style={[appStyles.subtitleHeadline2, { lineHeight: 40 }]}>
+                                    Habits
+                                </Text>
+                            </View>
+
+                            <Image
+                                source={plantImage}
+                                resizeMode="contain"
+                                style={{
+                                    position: 'absolute',
+                                    top: 70,
+                                    left: 80,
+                                    width: 313,
+                                    height: 348
+                                }}
+                            />
                         </View>
 
-                        <Image
-                            source={plantImage}
-                            resizeMode="contain"
-                            style={{
-                                position: 'absolute',
-                                top: 70,
-                                left: 80,
-                                width: 313,
-                                height: 348
-                            }}
-                        />
-                    </View>
+                        <View
+                            style={[
+                                appStyles.welcomeBlock,
+                                {
+                                    borderTopLeftRadius: 24,
+                                    borderTopRightRadius: 24,
+                                    paddingHorizontal: 24,
+                                    paddingTop: 25,
+                                    paddingBottom: 40,
+                                    marginTop: 259,
+                                    flexGrow: 1
+                                },
+                            ]}>
+                            
+                            <Text style={[
+                                appStyles.titleHeadline1,
+                                    {
+                                        textAlign: 'center',
+                                        fontSize: 60,
+                                    }
+                            ]}>
+                                Login
+                            </Text>
 
-                    <View
-                        style={[
-                            appStyles.welcomeBlock,
-                            {
-                                maxHeight: 420,
-                                borderTopLeftRadius: 24,
-                                borderTopRightRadius: 24,
-                                marginTop: 'auto',
-                                marginBottom: -40,
-                                paddingHorizontal: 24,
-                                paddingTop: 25
-                            },
-                        ]}>
-                        
-                        <Text style={[
-                            appStyles.titleHeadline1,
+                            <Text style={[
+                                appStyles.bodyHeadline4,
                                 {
                                     textAlign: 'center',
-                                    fontSize: 60,
-                                }
-                        ]}>
-                            Login
-                        </Text>
-
-                        <Text style={[
-                            appStyles.bodyHeadline4,
-                            {
-                                textAlign: 'center',
-                            },
-                        ]}>
-                            Dont Have an Account?{' '}
-                            <Text
-                                style={{ color: '#D1B3C4', fontWeight: '600' }}
-                                onPress={() => router.push('/(auth)/signup')}
-                            >
-                                Sign Up
+                                },
+                            ]}>
+                                Dont Have an Account?{' '}
+                                <Text
+                                    style={{ color: '#D1B3C4', fontWeight: '600' }}
+                                    onPress={() => router.push('/(auth)/signup')}
+                                >
+                                    Sign Up
+                                </Text>
                             </Text>
-                        </Text>
-                        
-                        <View style={[styles.verticallySpaced, styles.mt20]}>
-                            <View style={{ position: 'relative', justifyContent: 'center' }}>
-                                    <UserRound
+                            
+                            <View style={[styles.verticallySpaced, styles.mt20]}>
+                                <View style={{ position: 'relative', justifyContent: 'center' }}>
+                                        <UserRound
+                                            size={18}
+                                        color='#37423D80'
+                                            style={{ position: 'absolute', left: 20, zIndex: 1 }}
+                                        />
+                                        <TextInput
+                                            onChangeText={(text) => setEmail(text)}
+                                            value={email}
+                                            placeholder="Email"
+                                        placeholderTextColor='#37423D80'
+                                            autoCapitalize="none"
+                                            style={[
+                                                styles.input,
+                                                {
+                                                    backgroundColor: '#D5B9B280',
+                                                    borderRadius: 30,
+                                                    borderColor: '#D5B9B280',
+                                                    width: 358,
+                                                    height: 60,
+                                                    paddingLeft: 48
+                                                }
+                                            ]}
+                                        />
+                                </View>
+                            </View>
+
+                            <View style={styles.verticallySpaced}>
+                                <View style={{ position: 'relative', justifyContent: 'center' }}>
+                                    <LockKeyhole
                                         size={18}
-                                    color='#37423D80'
+                                        color='#37423D80'
                                         style={{ position: 'absolute', left: 20, zIndex: 1 }}
                                     />
                                     <TextInput
-                                        onChangeText={(text) => setEmail(text)}
-                                        value={email}
-                                        placeholder="Email"
-                                    placeholderTextColor='#37423D80'
+                                        onChangeText={(text) => setPassword(text)}
+                                        value={password}
+                                        secureTextEntry={true}
+                                        placeholder="Password"
+                                        placeholderTextColor='#37423D80'
                                         autoCapitalize="none"
                                         style={[
                                             styles.input,
@@ -132,64 +162,36 @@ export default function Login() {
                                             }
                                         ]}
                                     />
+                                </View>
                             </View>
-                        </View>
 
-                        <View style={styles.verticallySpaced}>
-                            <View style={{ position: 'relative', justifyContent: 'center' }}>
-                                <LockKeyhole
-                                    size={18}
-                                    color='#37423D80'
-                                    style={{ position: 'absolute', left: 20, zIndex: 1 }}
-                                />
-                                <TextInput
-                                    onChangeText={(text) => setPassword(text)}
-                                    value={password}
-                                    secureTextEntry={true}
-                                    placeholder="Password"
-                                    placeholderTextColor='#37423D80'
-                                    autoCapitalize="none"
+                            <View style={[styles.verticallySpaced, styles.mt20]}>
+                                <TouchableOpacity
                                     style={[
-                                        styles.input,
+                                        styles.button, loading && styles.buttonDisabled,
                                         {
-                                            backgroundColor: '#D5B9B280',
-                                            borderRadius: 30,
-                                            borderColor: '#D5B9B280',
-                                            width: 358,
                                             height: 60,
-                                            paddingLeft: 48
+                                            width: 358,
+                                            paddingTop: 16
                                         }
+
                                     ]}
-                                />
+                                    onPress={() => signInWithEmail()}
+                                    disabled={loading}
+                                >
+                                    <Text style={[
+                                        styles.subtitleHeadline4,
+                                        {
+                                            color: '#FFFFFF',
+                                            fontSize: 25
+                                        } 
+                                    ]}>Login</Text>
+                                </TouchableOpacity>
                             </View>
                         </View>
-
-                        <View style={[styles.verticallySpaced, styles.mt20]}>
-                            <TouchableOpacity
-                                style={[
-                                    styles.button, loading && styles.buttonDisabled,
-                                    {
-                                        height: 60,
-                                        width: 358,
-                                        paddingTop: 16
-                                    }
-
-                                ]}
-                                onPress={() => signInWithEmail()}
-                                disabled={loading}
-                            >
-                                <Text style={[
-                                    styles.subtitleHeadline4,
-                                    {
-                                        color: '#FFFFFF',
-                                        fontSize: 25
-                                    } 
-                                ]}>Login</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </ScrollView>
-            </KeyboardAvoidingView>   
-        </SafeAreaView>
+                    </ScrollView>
+                </KeyboardAvoidingView>   
+            </SafeAreaView>
+        </View>
     )
 }
